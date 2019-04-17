29bf661db4b99836eb3bc3d0ad5caca780cb94cd
arthurmmoura <45501623+arthurmmoura@users.noreply.github.com>
2019-04-16 22:37:59 -0300

    Add files via upload

--- SGA/db/conexao.php
+++ SGA/db/conexao.php
@@ -1,17 +1,20 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: tassio
- * Date: 09/01/2018
- * Time: 16:24
- */
 
-//ini_set('display_errors', 0);
-
+require_once "configDB.php";
+class conexao
+{
+private static $pdo;
+private function __construct() {
+self::getInstance();
+}
+public static function getInstance() {
+if (!isset(self::$pdo)) {
 try {
-    $pdo = new PDO('mysql:host=localhost;dbname=sga', 'root', 'root');
-    $pdo->exec("set names utf8");
-} catch ( PDOException $e ) {
-    echo 'Erro ao conectar com o Banco: ' . $e->getMessage();
-    exit(1);
+self::$pdo = new PDO(DRIVER.":host=" . HOSTNAME . "; dbname=" . DBNAME . "; charset=" . CHARSET . ";", USER, PASS);
+} catch (PDOException $e) {
+print "Erro: " . $e->getMessage();
+}
+}
+return self::$pdo;
+}
 }
---
+++ SGA/db/configDB.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alunoces
+ * Date: 15/04/2019
+ * Time: 21:33
+ */
+
+define("HOSTNAME","localhost");
+define("USER","root");
+define("PASS","root");
+define("DRIVER","mysql");
+define("DBNAME","sga");
+define('CHARSET', 'utf8');
+//define("PORT","3306");
